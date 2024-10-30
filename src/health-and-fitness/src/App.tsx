035cfaf42adb7222f828951f37240f639a1742ab
howtobefun <duytdn2806@gmail.com>
2024-10-30 21:01:12 +0700

    add more test

--- src/health-and-fitness/src/App.tsx
+++ src/health-and-fitness/src/App.tsx
@@ -30,6 +30,33 @@ function App() {
       }
     };
 
+    const createUser = async () => {
+      try {
+        const response = await fetch('https://intro2se-staging.vercel.app/api/user', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            username: 'exampleUser2',
+            password: '12345'
+
+          }),
+        });
+
+        // Check if the response is okay
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status}`);
+        }
+
+        const data = await response.json(); // Parse JSON response
+        setResponseData(data); // Update response data state
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    createUser();
     fetchData(); // Call the fetch function when component mounts
   }, []); // Empty dependency array to run only once
 
