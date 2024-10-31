f07ed6dbe9b041474d379711e7ba669f4b73d662
howtobefun <109527111+howtobefun@users.noreply.github.com>
2024-10-31 14:55:46 +0700

    Update vite.config.ts

--- src/health-and-fitness/vite.config.ts
+++ src/health-and-fitness/vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 9000, // Set the port to 3000 (or any other port you prefer)
-    strictPort: true, // Ensure Vite doesn't switch to another port if 3000 is occupied
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Add CORS headers to allow all origins
-    },
+    port: 9000
   },
 })
