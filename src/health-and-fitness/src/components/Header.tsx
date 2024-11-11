a3c0b437ada1de0aa580fe1a76ae18e9bc59cadb
ntpd164 <duongngo1616@gmail.com>
2024-11-12 00:53:29 +0700

    Add main text in header

--- src/health-and-fitness/src/components/Header.tsx
+++ src/health-and-fitness/src/components/Header.tsx
@@ -2,6 +2,10 @@ import Navbar from "./Navbar";
 import rectangle from "../assets/rectangle.png";
 import human from "../assets/human.png";
 import fitness from "../assets/fitness.png";
+import title from "../assets/title.png";
+import customize from "../assets/customize.png";
+import energy from "../assets/energy.png";
+import intensity from "../assets/intensity.png";
 
 export default function FitnessHeader() {
   return (
@@ -20,8 +24,38 @@ export default function FitnessHeader() {
       <img
         src={fitness}
         alt="Fitness"
-        className="absolute bottom-24 left-6 h-[70%]"
+        className="absolute bottom-28 left-6 h-[70%]"
       />
+      <div className="absolute top-40 left-32 space-y-8">
+        <img src={title} alt="Title" className="w-full" />
+        <div className="max-w-[40rem] text-lg">
+          <p>
+            HAF - The Health And Fitness platform where every limit is meant to
+            be conquered!
+          </p>
+          <p>
+            the key to breaking through personal boundaries, unlocking your
+            potential.
+          </p>
+        </div>
+        <div className="pt-6 uppercase bebas-font tracking-[0.2rem] text-xl">
+          <p>What you get</p>
+          <div className="flex mt-4">
+            <div className="flex justify-center items-center max-w-52 space-x-4">
+              <img src={customize} alt="" />
+              <p>Customised fitness plan</p>
+            </div>
+            <div className="flex justify-center items-center max-w-52 space-x-4">
+              <img src={energy} alt="" />
+              <p>Keep your feet and strong</p>
+            </div>
+            <div className="flex justify-center items-center max-w-52 space-x-4">
+              <img src={intensity} alt="" />
+              <p>High intensity training</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
