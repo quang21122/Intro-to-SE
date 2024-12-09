bd1487fa0cfee77f37b6b44689eaa089801aa277
MrTeeVee <thienvu0210@gmail.com>
2024-12-09 10:37:43 +0700

    set up not finish

--- src/health-and-fitness/src/vitest.setup.ts
+++ src/health-and-fitness/src/vitest.setup.ts
@@ -1,7 +1,20 @@
-import '@testing-library/jest-dom/vitest';
+import '@testing-library/jest-dom';
 import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
 
 afterEach(() => {
     cleanup();
 });
+
+if (!window.matchMedia) {
+    window.matchMedia = (query: string) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: () => {}, // Deprecated
+      removeListener: () => {}, // Deprecated
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => false,
+    });
+  }
