67379d9b5b16edfa58a8a2bc65bdaaf44d835c12
MrTeeVee <thienvu0210@gmail.com>
2024-12-27 14:08:14 +0700

    finish signup test case

--- src/health-and-fitness/src/pages/Signup.test.tsx
+++ src/health-and-fitness/src/pages/Signup.test.tsx
@@ -27,42 +27,7 @@ describe('Signup Page', () => {
         expect(screen.getByRole('button', { name: /sign up/i })).toBeInTheDocument();
     });
 
-    // let isAlreadySignedUp = false;
-    // it('should allow user to sign up with valid data', async () => {
-
-    //     const testEmail = 'john@gmail.com';
-
-    //     if (isAlreadySignedUp) {
-    //         console.log(`Email ${testEmail} đã được sử dụng trong lần test trước.`);
-    //         return; // Không chạy lại sign-up nếu email đã được sử dụng
-    //     }
-
-    //     render(
-    //         <MemoryRouter>
-    //             <AuthProvider>
-    //                 <ProfileProvider>
-    //                     <Signup />
-    //                 </ProfileProvider>
-    //             </AuthProvider>
-    //         </MemoryRouter>
-    //     );
-
-    //     await act(async () => {
-    //         fireEvent.change(screen.getByLabelText(/name/i), { target: { value: 'John Doe' } });
-    //         fireEvent.change(screen.getByLabelText(/email/i), { target: { value: testEmail } });
-    //         fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'password123' } });
-    //         fireEvent.change(screen.getByLabelText(/confirm password/i), { target: { value: 'password123' } });
-    //         fireEvent.click(screen.getByRole('button', { name: 'Sign Up' }));
-    //     });
-
-    //     waitFor(() => {
-    //         expect(localStorage.getItem('isAuthenticated')).toBe('true');
-    //     })
-    //     isAlreadySignedUp = true;
-    // });
-
-
-    it('should show error message for invalid email format', async () => {
+    it('should show error message for missing information', async () => {
         render(
             <MemoryRouter>
                 <AuthProvider>
@@ -74,11 +39,11 @@ describe('Signup Page', () => {
         );
 
         await act(async () => {
-            fireEvent.change(screen.getByLabelText(/email/i), { target: { value: 'invalid-email' } });
+            fireEvent.change(screen.getByLabelText(/email/i), { target: { value: 'email@gmail.com' } });
             fireEvent.click(screen.getByRole('button', { name: /sign up/i }));
         });
         await waitFor(() => {
-            expect(screen.getByText(/Invalid email/i)).toBeInTheDocument();
+            expect(screen.getByText(/fill in/i)).toBeInTheDocument();
         });
 
     });
@@ -96,6 +61,8 @@ describe('Signup Page', () => {
             </MemoryRouter>
         );
         await act(async () => {
+            fireEvent.change(screen.getByLabelText(/Username/i), { target: { value: 'hallo' } });
+            fireEvent.change(screen.getByLabelText(/email/i), { target: { value: 'email@gmail.com' } });
             fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'password123' } });
             fireEvent.change(screen.getByLabelText(/confirm password/i), { target: { value: 'password1234' } });
             fireEvent.click(screen.getByRole('button', { name: /sign up/i }));
